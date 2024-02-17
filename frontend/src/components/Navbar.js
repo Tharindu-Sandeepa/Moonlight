fb89c8fc7f6310fbf66c2c3efe4268e097464190
Tharindu Sandeepa <katharindusandeepa@gmail.com>
2024-02-17 19:03:26 +0530

    New Homepage design
    New nav bar change when scroll down
    a grid for items.
    new home.js file
    file structure change : new components file with all simple parts of the site

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,79 @@
+import React, { useState } from 'react';
+import { AppBar, Toolbar, Button, IconButton, Badge, useScrollTrigger } from '@mui/material';
+import { ShoppingCart } from '@mui/icons-material';
+import AccountMenu from './AccountMenu';
+import logo from '../img/logo.png';
+
+const Navbar = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  const trigger = useScrollTrigger({
+    target: window,
+    disableHysteresis: true,
+    threshold: 0,
+  });
+
+  React.useEffect(() => {
+    setIsScrolled(trigger);
+  }, [trigger]);
+
+  return (
+    <AppBar 
+      position="fixed" 
+      style={{ 
+        backgroundColor: isScrolled ? 'rgba(255, 255, 255, 0.8)' : 'transparent', 
+        backdropFilter: isScrolled ? 'blur(8px)' : 'none', 
+        transition: 'background-color 0.3s ease-out',
+        boxShadow: isScrolled ? '0 2px 4px rgba(0, 0, 0, 0.2)' : 'none'
+      }}
+    >
+      <Toolbar>
+        {/* Company Logo */}
+        <img src={logo} alt="Company Logo" style={{ height: '60px', marginRight: 'auto' }} />
+
+        {/* Navigation Buttons */}
+        <div className="navbar" style={{ marginRight: '50%' }}>
+          <Button color="inherit" href="#" sx={{ color: 'black', fontWeight: 'bold' }}>
+            Home
+          </Button>
+
+          <Button color="inherit" href="#" style={{ color: 'black', fontWeight: 'bold', marginLeft: "17px" }}>
+            Gems
+          </Button>
+
+          <Button color="inherit" href="#" style={{ color: 'black', fontWeight: 'bold', marginLeft: "17px" }}>
+            Jewelry
+          </Button>
+
+          <Button color="inherit" href="#" style={{ color: 'black', fontWeight: 'bold' }}>
+            About us
+          </Button>
+
+          
+        </div>
+
+        {/* Sign In, Sign Out Buttons */}
+        <Button color="inherit" href="#" sx={{ color: 'black', fontWeight: 'bold', marginRight: '10px' }}>
+          Sign In
+        </Button>
+        <Button color="inherit" href="#" sx={{ color: 'black', fontWeight: 'bold', marginRight: '10px' }}>
+          Sign Out
+        </Button>
+
+        {/* Cart Icon */}
+        <IconButton color="inherit" href="#" sx={{ color: 'black', fontWeight: 'bold' }}>
+          <Badge badgeContent={4} color="error">
+            <ShoppingCart />
+          </Badge>
+        </IconButton>
+
+        {/* AccountMenu - Positioned to the right */}
+        <div className="sublog">
+          <AccountMenu />
+        </div>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Navbar;
